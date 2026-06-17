38cf52438ff9d56d2539569aeeb787eea6b30f8a
theyfwBlvck <makanakii888@gmail.com>
2026-06-17 13:20:11 +0100

    create new file

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
 print("ooga booga")
 response = input("What do you want to say to the ooga booga? ")
 print("You said: " + response)
+
+print("who is the strongest")
+response = input(" ")
+print("SATORU GOJO !!!")
